1e8768ee8aee23e635063895c3b6152ad03a2b54
Oleksii <82764347+Oleksii-kh89@users.noreply.github.com>
2023-02-01 15:18:32 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,10 +1,3 @@
 def call() {
-    //sh "echo Hello World"
-  def chars = "abcdefghyjklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                    def run_id = "Test_"
-                        for (int i = 0; i < 15; i++){
-                            q = chars.charAt(new Random().nextInt(chars.length()))
-                            run_id = run_id+q
-                        }
-                    buildDescription "${run_id}"
+    sh "echo Hello World"
 }
