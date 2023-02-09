64256e2458eb8538adb36b9b13ecf2193b8e044f
Oleksii <82764347+Oleksii-kh89@users.noreply.github.com>
2023-02-09 15:45:55 +0200

    Update randomId.groovy

--- vars/randomId.groovy
+++ vars/randomId.groovy
@@ -1,9 +1,8 @@
 def call() {
   def chars = "abcdefghyjklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  def this.run_id = "Test_"
+  def run_id = "Test_"
       for (int i = 0; i < 15; i++){
          q = chars.charAt(new Random().nextInt(chars.length()))
          this.run_id = run_id+q
        }
-  return this.run_id
 }
