b14e6beab9c2b266c56633ba1fc8582ab38f84f1
Oleksii <82764347+Oleksii-kh89@users.noreply.github.com>
2023-02-09 15:14:43 +0200

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,6 @@
 def setTime() {
-    def time = new Date().getTime()
-    this.testS = time - 300000
+    def time = new Date().getTime();
+    this.testS = time - 300000;
 }
 def getTime() {
     return this.testS;
