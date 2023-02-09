87b13f8d1a0239424ddbf44a8009a21f67667427
Oleksii <82764347+Oleksii-kh89@users.noreply.github.com>
2023-02-09 15:39:32 +0200

    Create randomId.groovy

---
+++ vars/randomId.groovy
@@ -0,0 +1,9 @@
+def call() {
+  def chars = "abcdefghyjklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  def this.run_id = "Test_"
+      for (int i = 0; i < 15; i++){
+         q = chars.charAt(new Random().nextInt(chars.length()))
+         run_id = run_id+q
+       }
+  return this.run_id
+}
